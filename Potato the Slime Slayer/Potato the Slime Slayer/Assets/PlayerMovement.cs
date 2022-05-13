40761f8edc041f7125605bcd1a17bb5ff1c6e7c3
gabriela.wojcik <gabriela.wojcik@kajware.com>
2022-05-13 16:56:14 +0200

    Add Potato the Slime Slayer

---
+++ Potato the Slime Slayer/Potato the Slime Slayer/Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpForce;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+    }
+
+    void Move()
+    {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+        if (inputHorizontal > 0)
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        if (inputHorizontal < 0)
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        
+        float moveBy = inputHorizontal * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+
+        animator.SetFloat("Speed", Mathf.Abs(moveBy));
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
